e353e155f1947d465b6ccf60de3592e1a7200e4d
LeanOl <olmedo.leandro8@gmail.com>
2023-04-25 10:01:10 -0300

    Added Model Controls

---
+++ ObligatorioDA1/Interface/AddModel.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Interface
+{
+    partial class AddModel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.cbxFigure = new System.Windows.Forms.ComboBox();
+            this.cbxMaterial = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(45, 45);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(133, 20);
+            this.txtName.TabIndex = 0;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(42, 29);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Name";
+            // 
+            // cbxFigure
+            // 
+            this.cbxFigure.FormattingEnabled = true;
+            this.cbxFigure.Location = new System.Drawing.Point(45, 93);
+            this.cbxFigure.Name = "cbxFigure";
+            this.cbxFigure.Size = new System.Drawing.Size(121, 21);
+            this.cbxFigure.TabIndex = 2;
+            // 
+            // cbxMaterial
+            // 
+            this.cbxMaterial.FormattingEnabled = true;
+            this.cbxMaterial.Location = new System.Drawing.Point(45, 141);
+            this.cbxMaterial.Name = "cbxMaterial";
+            this.cbxMaterial.Size = new System.Drawing.Size(121, 21);
+            this.cbxMaterial.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Figure";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Material";
+            // 
+            // AddModel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbxMaterial);
+            this.Controls.Add(this.cbxFigure);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtName);
+            this.Name = "AddModel";
+            this.Size = new System.Drawing.Size(375, 244);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.ComboBox cbxFigure;
+        private System.Windows.Forms.ComboBox cbxMaterial;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ ObligatorioDA1/Interface/ModelList.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace Interface
+{
+    partial class ModelList
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddNewModel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAddNewModel
+            // 
+            this.btnAddNewModel.Location = new System.Drawing.Point(123, 39);
+            this.btnAddNewModel.Name = "btnAddNewModel";
+            this.btnAddNewModel.Size = new System.Drawing.Size(155, 23);
+            this.btnAddNewModel.TabIndex = 1;
+            this.btnAddNewModel.Text = "+ Add New Model";
+            this.btnAddNewModel.UseVisualStyleBackColor = true;
+            // 
+            // ModelList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnAddNewModel);
+            this.Name = "ModelList";
+            this.Size = new System.Drawing.Size(469, 284);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddNewModel;
+    }
+}
