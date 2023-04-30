559d40c241b8caa07b602f7cdb418c011d1216bb
LeanOl <olmedo.leandro8@gmail.com>
2023-04-30 11:26:57 -0300

    [Green] Add client to repository test pass

---
+++ ObligatorioDA1/RepositoryTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RepositoryTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RepositoryTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2b86e8ed-cfcd-4ad1-87f6-4cdf66c55755")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ObligatorioDA1/RepositoryTests/RepositoryTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Domain;
+using Repository;
+
+namespace RepositoryTests
+{
+    [TestClass]
+    public class RepositoryTest
+    {
+        private const string ValidUsername = "John";
+        private const string ValidUserPassword = "Abc12345";
+        private DateTime TestDate = DateTime.Now;
+        [TestMethod]
+        public void AddClient()
+        {
+            ClientRepository aRepository = new ClientRepository();
+            Client aClient = new Client()
+            {
+                Username = ValidUsername,
+                Password = ValidUserPassword,
+                RegisterDate = TestDate
+            };
+            aRepository.AddClient(aClient);
+            Assert.AreEqual(aRepository.GetClientByName(ValidUsername),aClient);
+        }
+    }
+}
