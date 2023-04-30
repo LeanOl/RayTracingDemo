c9919dd6b6ac8f7ce939ee5c99b43792f35613e5
LeanOl <olmedo.leandro8@gmail.com>
2023-04-30 00:40:38 -0300

    [Green] Username below char limit test pass

--- ObligatorioDA1/DomainLogicTest/ClientTests.cs
+++ ObligatorioDA1/DomainLogicTest/ClientTests.cs
@@ -1,34 +1,68 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using Domain;
+using Logic;
 
 namespace DomainLogicTest
 {
     [TestClass]
     public class ClientTests
     {
-        private const string TestName = "John";
-        private const string TestPassword = "Abc12345";
+        private ClientLogic logic;
+        private const string ValidName = "John";
+        private const string NameBelowCharLimit = "A5";
+        private const string ValidPassword = "Abc12345";
         private DateTime TestDate = DateTime.Now;
+        private const string UsernameBelowCharLimitMessage = "Username has to be at least 3 characters long";
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            logic = new ClientLogic();
+        }
         [TestMethod]
         public void CreateClientSuccessfully()
         {
 
             Client aClient = new Client()
             {
-                Username = TestName,
-                Password = TestPassword,
+                Username = ValidName,
+                Password = ValidPassword,
                 RegisterDate = TestDate
             };
             
             Assert.IsNotNull(aClient);
-            Assert.AreEqual(aClient.Username,TestName);
-            Assert.AreEqual(aClient.Password,TestPassword);
+            Assert.AreEqual(aClient.Username,ValidName);
+            Assert.AreEqual(aClient.Password,ValidPassword);
             Assert.AreEqual(aClient.RegisterDate,TestDate);
 
         }
-
        
+        [TestMethod]
+        public void ClientUsernameBelowCharacterLimit_ThrowException()
+        {
+
+            Client aClient = new Client()
+            {
+                Username = NameBelowCharLimit,
+                Password = ValidPassword,
+                RegisterDate = TestDate
+            };
+            logic = new ClientLogic();
+            try
+            {
+                logic.CreateClient(aClient);
+                Assert.Fail("Should throw exception");
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual(ex.Message,UsernameBelowCharLimitMessage);
+            }
+
+
+
+        }
+
     }
 
   
---
+++ ObligatorioDA1/Logic/ClientLogic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Logic
+{
+    public class ClientLogic
+    {
+        public void CreateClient(Client aClient)
+        {
+            if (aClient.Username.Length < 3)
+            {
+                throw new ArgumentException( "Username has to be at least 3 characters long");
+            }
+        }
+    }
+}
