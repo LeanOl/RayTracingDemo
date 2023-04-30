f79581da663476e6b5a1c0768e00eb4a0cb8f069
LeanOl <olmedo.leandro8@gmail.com>
2023-04-30 00:00:06 -0300

    [Green] Create client successfully test pass

---
+++ ObligatorioDA1/DomainLogicTest/ClientTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Domain;
+
+namespace DomainLogicTest
+{
+    [TestClass]
+    public class ClientTests
+    {
+        private const string TestName = "John";
+        private const string TestPassword = "Abc12345";
+        private DateTime TestDate = DateTime.Now;
+        [TestMethod]
+        public void CreateClientSuccessfully()
+        {
+
+            Client aClient = new Client()
+            {
+                Username = TestName,
+                Password = TestPassword,
+                RegisterDate = TestDate
+            };
+            
+            Assert.IsNotNull(aClient);
+            Assert.AreEqual(aClient.Username,TestName);
+            Assert.AreEqual(aClient.Password,TestPassword);
+            Assert.AreEqual(aClient.RegisterDate,TestDate);
+
+        }
+
+       
+    }
+
+  
+}
