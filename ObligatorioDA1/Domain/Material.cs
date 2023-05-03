64f1742fb91c813c78f519125438333ae91f61e3
LeanOl <olmedo.leandro8@gmail.com>
2023-05-03 19:08:54 -0300

    [Green] Create material ok test pass

---
+++ ObligatorioDA1/Domain/Material.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+
+namespace Domain
+{
+    public class Material
+    {
+        public Client Owner { get; set; }
+        public string Name { get; set; }
+        public Color Color { get; set; }
+    }
+}
---
+++ ObligatorioDA1/DomainTest/MaterialTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DomainTest
+{
+    [TestClass]
+    public class MaterialTests
+    {
+        private Client aClient;
+        [TestInitialize]
+        public void Initialize()
+        {
+            const string ValidUsername = "John";
+            const string ValidPassword = "Abc12345";
+            DateTime testDate = DateTime.Now;
+
+            aClient = new Client()
+            {
+                Username = ValidUsername,
+                Password = ValidPassword,
+                RegisterDate = testDate
+            };
+
+        }
+        [TestMethod]
+        public void CreateMaterialSuccessfully()
+        {
+            Color materialColor = Color.FromArgb(205, 215, 235);
+            Material aMaterial = new Material()
+            {
+                Owner = aClient,
+                Name = "New Color",
+                Color= materialColor
+
+            };
+            Assert.IsNotNull(aMaterial);
+            Assert.AreEqual(materialColor, aMaterial.Color);
+            Assert.AreEqual(aClient,aMaterial.Owner);
+            
+        }
+    }
+}
