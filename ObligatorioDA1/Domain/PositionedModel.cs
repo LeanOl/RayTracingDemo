2b9028d2667bb7a0c555e16eb63f9ee3601e6d73
LeanOl <olmedo.leandro8@gmail.com>
2023-05-06 18:06:01 -0300

    [Green] Create positioned model object test pass

---
+++ ObligatorioDA1/Domain/PositionedModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain
+{
+    public class PositionedModel
+    {
+        public Vector Position { get; set; }
+        public Model Model { get; set; }
+    }
+}
---
+++ ObligatorioDA1/DomainTest/PositionedModelTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Domain;
+
+namespace DomainTest
+{
+    [TestClass]
+    public class PositionedModelTest
+    {
+        [TestMethod]
+        public void CreatePositionedModelSuccessfully()
+        {
+            PositionedModel positionedModel = new PositionedModel()
+            {
+                Model = new Model(),
+                Position = new Vector()
+            };
+            Assert.IsNotNull(positionedModel);
+        }
+    }
+}
