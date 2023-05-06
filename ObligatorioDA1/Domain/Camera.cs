4d2cbd285de2bfa33101c76eacffd73e2c0376da
LeanOl <olmedo.leandro8@gmail.com>
2023-05-06 18:08:19 -0300

    [Green] Create camera object test pass

---
+++ ObligatorioDA1/Domain/Camera.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain
+{
+    public class Camera
+    {
+        public Vector LookFrom { get; set; }
+        public Vector LookAt { get; set; }
+        public Vector Up { get; set; }
+        public int FieldOfView { get; set; }
+        public int AspectRatio { get; set; }
+    }
+}
---
+++ ObligatorioDA1/DomainTest/CameraTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Domain;
+
+namespace DomainTest
+{
+    [TestClass]
+    public class CameraTests
+    {
+        [TestMethod]
+        public void CreateCameraSuccessfully()
+        {
+            Camera camera = new Camera()
+            {
+                LookFrom = new Vector(),
+                LookAt = new Vector(),
+                Up = new Vector(),
+                FieldOfView = 1,
+                AspectRatio = 1
+            };
+            Assert.IsNotNull(camera);
+        }
+    }
+}
