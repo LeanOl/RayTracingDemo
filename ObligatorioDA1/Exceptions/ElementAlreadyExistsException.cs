aaa3f04714e622107194cdea96c12a17e490d2c3
GabrielGuerra404 <gabrielguerra404@gmail.com>
2023-05-05 23:51:24 -0300

    Added Exceptions project and an exception

---
+++ ObligatorioDA1/Exceptions/ElementAlreadyExistsException.cs
@@ -0,0 +1,13 @@
+﻿namespace Exceptions
+{
+    public class ElementAlreadyExistsException : Exception
+    {
+        public ElementAlreadyExistsException() { }
+
+        public ElementAlreadyExistsException(string message)
+            : base(message) { }
+
+        public ElementAlreadyExistsException(string message, Exception inner)
+            : base(message, inner) { }
+    }
+}
