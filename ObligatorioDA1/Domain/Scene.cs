2e9d016b6328a5d426c34f8fd55078cc767530b7
LeanOl <olmedo.leandro8@gmail.com>
2023-05-06 18:08:38 -0300

    [Green] Create Scene object test pass

---
+++ ObligatorioDA1/Domain/Scene.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Domain
+{
+    public class Scene
+    {
+        public Image Preview { get; set; }
+        public Client Proprietary { get; set; }
+        public string Name { get; set; }
+        public DateTime CreationDate { get; set; }
+        public List<PositionedModel> ModelList { get; set; }
+        public Camera Camera { get; set; }
+        public DateTime LastModified { get; set; }
+        public DateTime LastRendered { get; set; }
+    }
+}
---
+++ ObligatorioDA1/DomainTest/SceneTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Domain;
+
+namespace DomainTest
+{
+    [TestClass]
+    public class SceneTests
+    {
+        [TestMethod]
+        public void CreateSceneSuccessfully()
+        {
+            Image preview = null;
+            Scene scene = new Scene()
+            {
+                Proprietary = new Client(),
+                Name = "Scene",
+                CreationDate = DateTime.Now,
+                ModelList= new List<PositionedModel>(),
+                Camera = new Camera(),
+                LastModified= DateTime.Now,
+                LastRendered= DateTime.Now,
+                Preview= preview
+                
+            };
+        }
+    }
+}
