d36852ce402ec4782c56407cda7f359b3293d1d2
GabrielGuerra404 <gabrielguerra404@gmail.com>
2023-05-05 22:13:58 -0300

    [RED] CreateFigure, FigureExists

---
+++ ObligatorioDA1/DomainLogicTest/FigureLogicTests.cs
@@ -0,0 +1,75 @@
+﻿using Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DomainLogicTest
+{
+    [TestClass]
+    public class FigureLogicTests
+    {
+        private Client aClient;
+        private Figure aFigure;
+        private const string ValidFigureName = "Ball";
+        private const string ValidUsername = "John";
+        private int ValidRadius = 5;
+
+        [TestInitialize]
+        public void initialize()
+        {
+            aClient = new Client()
+            {
+                Username = ValidUsername
+            };
+            aFigure = new Sphere()
+            {
+                Propietary = aClient,
+                Name = ValidFigureName,
+                Radius = ValidRadius
+            };
+        }
+
+        [TestMethod]
+        public void CreateFigureTestAndCheckIfItExists()
+        {
+            Exception exceptionCaught = null;
+            bool figureWasCreated = false;
+            FigureLogic logic = new FigureLogic();
+
+            try
+            {
+                logic.CreateFigure(aFigure);
+
+                figureWasCreated = logic.FigureExists(aFigure.Name);
+            }catch (Exception ex)
+            {
+                exceptionCaught = ex;
+            }
+
+            Assert.IsNull(exceptionCaught);
+            Assert.IsTrue(figureWasCreated);
+        }
+
+        public void CheckIfFigureDoesNotExist()
+        {
+            Exception exceptionCaught = null;
+
+            //Initialized on true to prevent the test from succeding on accident
+            bool figureWasCreated = true; 
+
+            FigureLogic logic = new FigureLogic();
+
+            try
+            {
+                figureWasCreated = logic.FigureExists(aFigure.Name);
+            }
+            catch (Exception ex)
+            {
+                exceptionCaught = ex;
+            }
+
+            Assert.IsNull(exceptionCaught);
+            Assert.IsFalse(figureWasCreated);
+        }
+
+    }
+}
--- ObligatorioDA1/Logic/FigureLogic.cs
+++ ObligatorioDA1/Logic/FigureLogic.cs
@@ -1,7 +1,27 @@
-﻿namespace DomainLogicTest
+﻿using Domain;
+using Repository;
+using System;
+
+namespace DomainLogicTest
 {
     public class FigureLogic
     {
+        private FigureRepository _repository;
+
+        public FigureLogic()
+        {
+            FigureRepository _repository = new FigureRepository();
+        }
+
+        public void CreateFigure(Figure aFigure)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool FigureExists(string name)
+        {
+            throw new NotImplementedException();
+        }
 
     }
 }
