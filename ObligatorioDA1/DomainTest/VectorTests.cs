e875c86d0233b37ddef9e1ea79b3de610e90c575
LeanOl <olmedo.leandro8@gmail.com>
2023-05-06 18:05:11 -0300

    [Green] Create Vector object test pass

---
+++ ObligatorioDA1/DomainTest/VectorTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Domain;
+
+namespace DomainTest
+{
+    [TestClass]
+    public class VectorTests
+    {
+        [TestMethod]
+        public void CreateVectorSuccessfully()
+        {
+            Vector vector = new Vector()
+            {
+                X = 1,
+                Y = 2,
+                Z = 3
+            };
+            Assert.IsNotNull(vector);
+        }
+    }
+}
