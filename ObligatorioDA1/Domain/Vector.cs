115cc7a949cdcf6c51030a9269ae010f993cbb66
LeanOl <olmedo.leandro8@gmail.com>
2023-05-06 18:24:40 -0300

    [Green] Add two vectors test pass

--- ObligatorioDA1/Domain/Vector.cs
+++ ObligatorioDA1/Domain/Vector.cs
@@ -5,5 +5,15 @@
         public int X { get; set; }
         public int Y { get; set; }
         public int Z { get; set; }
+
+        public Vector Add(Vector vector2)
+        {
+            return new Vector()
+            {
+                X = X + vector2.X,
+                Y = Y + vector2.Y,
+                Z = Z + vector2.Z
+            };
+        }
     }
 }
--- ObligatorioDA1/DomainTest/VectorTests.cs
+++ ObligatorioDA1/DomainTest/VectorTests.cs
@@ -18,5 +18,26 @@ namespace DomainTest
             };
             Assert.IsNotNull(vector);
         }
+
+        [TestMethod]
+        public void AddTwoVectorsSuccessfully()
+        {
+            Vector vector1 = new Vector()
+            {
+                X = 1,
+                Y = 2,
+                Z = 3
+            };
+            Vector vector2 = new Vector()
+            {
+                X = 1,
+                Y = 2,
+                Z = 3
+            };
+            Vector vector3 = vector1.Add(vector2);
+            Assert.AreEqual(2, vector3.X);
+            Assert.AreEqual(4, vector3.Y);
+            Assert.AreEqual(6, vector3.Z);
+        }   
     }
 }
