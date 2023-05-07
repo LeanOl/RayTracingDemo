55bdac53e0f37e235763584c95390b6cd36b317b
LeanOl <olmedo.leandro8@gmail.com>
2023-05-07 19:07:01 -0300

    [Green] Graphics engine default values test pass

---
+++ ObligatorioDA1/Domain/GraphicsEngine.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain
+{
+    public class GraphicsEngine
+    {
+        public int Resolution { get; set; } = 200;
+        public int SamplesPerPixel { get; set; } = 50;
+        public int MaxDepth { get; set; } = 20;
+    }
+}
---
+++ ObligatorioDA1/DomainTest/GraphicsEngineTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Domain;
+
+namespace DomainTest
+{
+    [TestClass]
+    public class GraphicsEngineTests
+    {
+        [TestMethod]
+        public void CreateGraphicsEngineDefaultValuesOk()
+        {
+            GraphicsEngine engine = new GraphicsEngine();
+            Assert.IsNotNull(engine);
+            Assert.AreEqual(200, engine.Resolution);
+            Assert.AreEqual(50, engine.SamplesPerPixel);
+            Assert.AreEqual(20,engine.MaxDepth);
+        }
+    }
+}
