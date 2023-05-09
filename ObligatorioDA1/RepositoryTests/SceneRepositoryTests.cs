8ce575038701dd039afb544a8ac4e60b96ce231e
LeanOl <olmedo.leandro8@gmail.com>
2023-05-09 09:56:19 -0300

    [Green] Scene repository scene add test pass

---
+++ ObligatorioDA1/RepositoryTests/SceneRepositoryTests.cs
@@ -0,0 +1,35 @@
+﻿using Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Repository;
+using System;
+
+namespace RepositoryTests
+{
+    [TestClass]
+    public class SceneRepositoryTests
+    {
+
+        private Client _someClient;
+        private SceneRepository _repository;
+        [TestMethod]
+        public void AddSeneSuccessfully()
+        {
+            _repository= new SceneRepository();
+            const string username = "John";
+            _someClient = new Client()
+            {
+                Username = username,
+            };
+            Scene testScene = new Scene
+            {
+                Name= "New Scene"
+            };
+            _repository.Add(testScene);
+            Assert.AreEqual(testScene, _repository.GetByName("New Scene"));
+
+        }
+    }
+}
+
+
+
