7e4c4560ac8e3e8f6fd32c52e4f6138741daf593
LeanOl <olmedo.leandro8@gmail.com>
2023-05-09 10:49:26 -0300

    [Green] Scene logic create empty scene test pass

---
+++ ObligatorioDA1/DomainLogicTest/SceneLogicTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Domain;
+using Logic;
+
+namespace DomainLogicTest
+{
+    [TestClass]
+    public class SceneLogicTests
+    {
+        SceneLogic _logic = new SceneLogic();
+        private Client _proprietary = new Client { Username = "John" };
+        [TestMethod]
+        public void CreateEmptySceneSuccessfully()
+        {
+            _logic.CreateEmptyScene(_proprietary);
+            Assert.IsNotNull(_logic.GetSceneByName("Empty Scene"));
+        }
+    }
+}
---
+++ ObligatorioDA1/Logic/SceneLogic.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using Domain;
+using Repository;
+
+namespace Logic
+{
+    public class SceneLogic
+    {
+        private SceneRepository _repository = new SceneRepository();
+        public void CreateEmptyScene(Client proprietary)
+        {
+            Camera sceneCamera = new Camera();
+            Bitmap defaultPreview = new Bitmap(50, 50);
+            Graphics gfx = Graphics.FromImage(defaultPreview);
+            gfx.FillRectangle(Brushes.Gray, 0, 0, 50, 50);
+            Scene emptyScene = new Scene
+            {
+                Name = "Empty Scene",
+                Proprietary = proprietary,
+                Camera = sceneCamera,
+                CreationDate = System.DateTime.Now,
+                LastModified = System.DateTime.Now,
+                LastRendered = System.DateTime.Now,
+                ModelList = new List<PositionedModel>(),
+                Preview = defaultPreview
+            };
+            _repository.Add(emptyScene);
+            
+        }
+
+        public Scene GetSceneByName(string emptyScene)
+        {
+            return _repository.GetByName(emptyScene);
+        }
+    }
+}
