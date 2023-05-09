8949a66743e173e4f1849803bdd4523ea3c594b8
LeanOl <olmedo.leandro8@gmail.com>
2023-05-08 21:48:42 -0300

    [Green] Obtain color test pass

--- ObligatorioDA1/Domain/GraphicsEngine.cs
+++ ObligatorioDA1/Domain/GraphicsEngine.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
 
 namespace Domain
 {
@@ -40,6 +42,51 @@ namespace Domain
                     throw new ArgumentOutOfRangeException();
                 _maxDepth = value;
             }
+        }
+
+        public Color ObtainColor(Ray ray, int depth, List<PositionedModel> elements)
+        {
+            HitRecord hitRecord = null;
+            PositionedModel hitModel = null;
+            decimal tMax = decimal.MaxValue; 
+            foreach (PositionedModel model in elements)
+            {
+                HitRecord hit = model.Model.Figure.Hit(ray, 0.001m, tMax, model.Position);
+                if (hit != null)
+                {
+                    tMax = hit.T;
+                    hitRecord = hit;
+                    hitModel = model;
+                }
+            }
+
+            if (hitRecord != null)
+            {
+                if (depth > 0)
+                {
+                    Ray scatteredRay = hitModel.Model.Material.Scatter(hitRecord);
+                    Color color = ObtainColor(scatteredRay, depth - 1, elements);
+                    Color attenuation = hitModel.Model.Material.Color;
+                    int red= (int) (((color.R / 255m) * (attenuation.R / 255m))*255);
+                    int green= (int) (((color.G / 255m) * (attenuation.G / 255m))*255);
+                    int blue= (int) (((color.B / 255m) * (attenuation.B / 255m))*255);
+                    return Color.FromArgb(red,green,blue);
+                }
+                else
+                {
+                    return Color.FromArgb(0, 0, 0);
+                }
+            }
+            else
+            {
+                Vector directionUnit=ray.Direction.Unit();
+                decimal posY = 0.5m * (directionUnit.Y + 1);
+                Vector colorStart=new Vector { X=1,Y=1,Z=1};
+                Vector colorEnd=new Vector { X=0.5m,Y=0.7m,Z=1};
+                Vector colorToReturn = colorStart.Multiply(1-posY).Add(colorEnd.Multiply(posY));
+                return Color.FromArgb((int)(colorToReturn.X*255),(int)(colorToReturn.Y*255),(int)(colorToReturn.Z*255));
+                
+            };
         }
     }
 }
--- ObligatorioDA1/DomainTest/GraphicsEngineTests.cs
+++ ObligatorioDA1/DomainTest/GraphicsEngineTests.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
+using System.Drawing;
 using Domain;
 
 namespace DomainTest
@@ -36,6 +38,29 @@ namespace DomainTest
             GraphicsEngine engine = new GraphicsEngine();
             Assert.ThrowsException<ArgumentOutOfRangeException>(() => engine.MaxDepth = 0);
         }
-        
+
+        [TestMethod]
+        public void ObtainColorSuccessfully()
+        {
+            Material aLamberitan = new Lambertian { Color = Color.FromArgb(25, 51, 127) };
+            Figure aSphere = new Sphere { Radius = 0.5m};
+            Model aModel=new Model { Figure=aSphere,Material=aLamberitan};
+            PositionedModel aPositionedModel=new PositionedModel { Model=aModel,Position=new Vector { X=0,Y=0.5m,Z=-2}};
+            List<PositionedModel> elements = new List<PositionedModel>
+            {
+                aPositionedModel
+            };
+            GraphicsEngine engine = new GraphicsEngine();
+            Ray ray = new Ray{Origin=new Vector{X=4,Y=2,Z=8},Direction=new Vector{X=-0.37470m,Y=-0.09534m,Z=-0.92324m}};
+
+            Color color = engine.ObtainColor(ray,2,elements);
+            Assert.IsNotNull(color);
+            int tolerance=5;
+            Assert.IsTrue(Math.Abs(color.R - 13) < tolerance);
+            Assert.IsTrue(Math.Abs(color.G - 36) < tolerance);
+            Assert.IsTrue(Math.Abs(color.B - 127) < tolerance);
+        }
+       
+
     }
 }
