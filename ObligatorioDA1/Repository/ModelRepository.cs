9701605be9bef0716488eeaa34e82072106d29b0
LeanOl <olmedo.leandro8@gmail.com>
2023-05-10 10:42:17 -0300

    [Green] Add model to repository test pass

---
+++ ObligatorioDA1/Repository/ModelRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Domain;
+
+namespace Repository
+{
+    public class ModelRepository
+    {
+        private List<Model> models=new List<Model>();
+        public Model GetModelByName(string name)
+        {
+            return models.FirstOrDefault(model => model.Name == name);
+        }
+
+        public void AddModel(Model model)
+        {
+            models.Add(model);
+        }
+    }
+}
---
+++ ObligatorioDA1/RepositoryTests/ModelRepositoryTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Domain;
+using Repository;
+
+namespace RepositoryTests
+{
+    [TestClass]
+    public class ModelRepositoryTests
+    {
+        private ModelRepository _repository;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _repository = new ModelRepository();
+        }
+        [TestMethod]
+        public void AddModelToRepositoryOk()
+        {
+            Model testModel = new Model{Name = "Model"};
+            _repository.AddModel(testModel);
+            Assert.AreEqual(testModel,_repository.GetModelByName("Model"));
+
+        }
+    }
+}
