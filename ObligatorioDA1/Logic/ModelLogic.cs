cef6ae324c6491825f84df476e1ee2fd0aa7c373
LeanOl <olmedo.leandro8@gmail.com>
2023-05-10 12:01:01 -0300

    [Refactor] added const for error message

--- ObligatorioDA1/Logic/ModelLogic.cs
+++ ObligatorioDA1/Logic/ModelLogic.cs
@@ -9,12 +9,14 @@ namespace Logic
     {
         private ModelRepository _modelRepository = new ModelRepository();
         private const string EmptyNameMessage = "Model name should not be empty";
-        
+        private const string NullFigureMessage = "Figure should not be null";
+        private const string DulplicateNameMessage = "There is already a model with this name";
+        private const string NameEndsOrStartsWhitespaceMessage = "Model name should not start or end with whitespaces";
+
         public void CreateModel(string name, Client proprietary, Figure figure, Material material)
         {
             ValidateName(name, proprietary);
-            if(figure == null)
-                throw new ArgumentException("Figure should not be null");
+            ValidateFigure(figure);
             Model model = new Model()
             {
                 Name = name,
@@ -23,6 +25,12 @@ namespace Logic
                 Material = material
             };
             _modelRepository.AddModel(model);
+        }
+
+        private static void ValidateFigure(Figure figure)
+        {
+            if (figure == null)
+                throw new ArgumentException(NullFigureMessage);
         }
 
         private void ValidateName(string name, Client proprietary)
@@ -37,13 +45,13 @@ namespace Logic
             List<Model> clientModels= _modelRepository.GetClientModels(proprietary);
             bool isNameDuplicated = clientModels.Exists(m => m.Name == name);
             if (isNameDuplicated)
-                throw new ArgumentException("There is already a model with this name");
+                throw new ArgumentException(DulplicateNameMessage);
         }
 
         private static void ValidateWhitespaces(string name)
         {
             if (name.StartsWith(" ") || name.EndsWith(" "))
-                throw new ArgumentException("Model name should not start or end with whitespaces");
+                throw new ArgumentException(NameEndsOrStartsWhitespaceMessage);
         }
 
         private static void ValidateEmptyName(string model1)
