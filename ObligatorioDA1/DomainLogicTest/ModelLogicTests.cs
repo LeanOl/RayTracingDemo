fbd11056bd5b2f0bf9f310c7d8f411a73f8ec6be
LeanOl <olmedo.leandro8@gmail.com>
2023-05-10 11:08:47 -0300

    [Green] Create model Ok test pass

---
+++ ObligatorioDA1/DomainLogicTest/ModelLogicTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Domain;
+using Logic;
+
+namespace DomainLogicTest
+{
+    [TestClass]
+    public class ModelLogicTests
+    {
+        private ModelLogic _modelLogic;
+        private Client _proprietary;
+        private Figure _figure;
+        private Material _material;
+        [TestInitialize]
+        public void Initialize()
+        {
+            _proprietary = new Client{Username = "John"};
+            _modelLogic = new ModelLogic();
+            _figure = new Sphere{ Name = "Figure1"};
+            _material = new Lambertian { Name = "Material1"};
+
+        }
+        [TestMethod]
+        public void CreateModelOk()
+        {
+            _modelLogic.CreateModel("Model1", _proprietary,_figure,_material);
+            Assert.IsNotNull(_modelLogic.GetModelByName("Model1"));
+        }
+    }
+}
---
+++ ObligatorioDA1/Logic/ModelLogic.cs
@@ -0,0 +1,26 @@
+﻿using Domain;
+using Repository;
+
+namespace Logic
+{
+    public class ModelLogic
+    {
+        private ModelRepository _modelRepository = new ModelRepository();
+        public void CreateModel(string model1, Client proprietary, Figure figure, Material material)
+        {
+            Model model = new Model()
+            {
+                Name = model1,
+                Proprietary = proprietary,
+                Figure = figure,
+                Material = material
+            };
+            _modelRepository.AddModel(model);
+        }
+
+        public Model GetModelByName(string model1)
+        {
+            return _modelRepository.GetModelByName(model1);
+        }
+    }
+}
