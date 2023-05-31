8109d6232af9e2de185c746d91ad650f6dc240ad
LeanOl <olmedo.leandro8@gmail.com>
2023-05-31 20:04:57 -0300

    [Red] Render Metallic test creation

---
+++ ObligatorioDA1/Domain/Materials/Metallic.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain
+{
+    public class Metallic:Material
+    {
+        public decimal Roughness { get; set; }
+        public override Ray Scatter(HitRecord hitRecord)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
