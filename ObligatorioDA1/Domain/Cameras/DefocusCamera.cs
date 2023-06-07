022340926fed73a6bb20379d023f573df5e4acac
LeanOl <olmedo.leandro8@gmail.com>
2023-06-07 12:40:17 -0300

    [Green] GetRay with defocus test pass

---
+++ ObligatorioDA1/Domain/Cameras/DefocusCamera.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Domain
+{
+    public class DefocusCamera : Camera
+    {
+        private decimal _lensRadius;
+        private decimal _focalDistance;
+        private Vector _vectorU;
+        private Vector _vectorV;
+        public Random RandomGenerator { get; set; } = new Random();
+
+        private void CalculateValues()
+        {
+            _focalDistance = LookFrom.Subtract(LookAt).Length();
+            _lensRadius = (decimal)Aperture / 2;
+            decimal theta = FieldOfView * (decimal)Math.PI / 180;
+            decimal heightHalf = (decimal)Math.Tan((double)theta / 2);
+            decimal widthHalf = (decimal)AspectRatio * heightHalf;
+            Vector vectorW = LookFrom.Subtract(LookAt).Unit();
+            Vector vectorU = Up.CrossProduct(vectorW).Unit();
+            Vector vectorV = vectorW.CrossProduct(vectorU);
+            _cornerLowerLeft = LookFrom.Subtract(vectorU.Multiply(widthHalf * _focalDistance))
+                .Subtract(vectorV.Multiply(heightHalf * _focalDistance))
+                .Subtract(vectorW.Multiply(_focalDistance));
+            _horizontal = vectorU.Multiply(2 * widthHalf * _focalDistance);
+            _vertical = vectorV.Multiply(2 * heightHalf * _focalDistance);
+            _vectorU = vectorU;
+            _vectorV = vectorV;
+            
+        }
+
+        public override Ray GetRay(decimal u, decimal v)
+        {
+            CalculateValues();
+            Vector randomVector = Vector.RandomInUnitSphere(RandomGenerator).Multiply(_lensRadius);
+            Vector offset = _vectorU.Multiply(randomVector.X).Add(_vectorV.Multiply(randomVector.Y));
+            Vector horizontalPosition = _horizontal.Multiply(u);
+            Vector verticalPosition = _vertical.Multiply(v);
+
+            return new Ray
+            {
+                Origin = LookFrom.Add(offset),
+                Direction = _cornerLowerLeft.Add(horizontalPosition.Add(verticalPosition)).Subtract(LookFrom).Subtract(offset)
+            };
+        }
+    }
+}
--- ObligatorioDA1/DomainTest/CameraTests.cs
+++ ObligatorioDA1/DomainTest/CameraTests.cs
@@ -41,6 +41,29 @@ namespace DomainTest
             Assert.AreEqual(-1.10139m, Math.Truncate(testRay.Direction.Z * fiveDecimals) / fiveDecimals);
 
         }
+        [TestMethod]
+        public void GetRayFromDefocusCameraSuccessfully()
+        {
+            Camera testCamera = new DefocusCamera
+            {
+                LookFrom = new Vector { X = 4, Y = 2, Z = 8 },
+                LookAt = new Vector { X = 0, Y = 1m, Z = -2 },
+                FieldOfView = 30,
+                AspectRatio = 75d / 50d,
+                Aperture = 0.5,
+                RandomGenerator = new Random(456)
+            };
+
+            int fiveDecimals = 100000;
+            Ray testRay = testCamera.GetRay(1, 1);
+            Assert.AreEqual(4.20792m, Math.Truncate(testRay.Origin.X * fiveDecimals)/fiveDecimals);
+            Assert.AreEqual(2.05785m, Math.Truncate(testRay.Origin.Y* fiveDecimals)/fiveDecimals);
+            Assert.AreEqual(7.91104m, Math.Truncate(testRay.Origin.Z * fiveDecimals) / fiveDecimals);
+            Assert.AreEqual(-0.27091m, Math.Truncate(testRay.Direction.X * fiveDecimals) / fiveDecimals);
+            Assert.AreEqual(1.82804m, Math.Truncate(testRay.Direction.Y * fiveDecimals) / fiveDecimals);
+            Assert.AreEqual(-11.77443m, Math.Truncate(testRay.Direction.Z * fiveDecimals) / fiveDecimals);
+
+        }
 
     }
 }
