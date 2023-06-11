33944862f23626d2b3164ab99acb193e97aa1808
LeanOl <olmedo.leandro8@gmail.com>
2023-06-11 17:07:57 -0300

    [Green] AddMaterialSuccessfully test pass

---
+++ ObligatorioDA1/Repository/DBRepository/MaterialDBRepository.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Domain;
+
+namespace Repository.DBRepository
+{
+    public class MaterialDBRepository : IMaterialRepository
+    {
+        private RayTracingContext _context;
+        public MaterialDBRepository(RayTracingContext context)
+        {
+            _context = context;
+        }
+
+        public MaterialDBRepository()
+        {
+            _context = new RayTracingContext();
+        }
+
+
+        public void Add(Material someMaterial)
+        {
+            _context.Materials.Add(someMaterial);
+            _context.SaveChanges();
+        }
+
+        public Material GetByName(string name)
+        {
+            return _context.Materials.FirstOrDefault(material => material.Name == name);
+        }
+
+        public List<Material> GetMaterialsByClient(Client someClient)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(Material materialToDelete)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ ObligatorioDA1/RepositoryTests/DBRepositoryTests/MaterialDBRepositoryTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Repository.DBRepository;
+using Repository;
+using System;
+using System.Data.Entity;
+using System.Drawing;
+using Domain;
+
+namespace RepositoryTests.DBRepositoryTests
+{
+    [TestClass]
+    public class MaterialDBRepositoryTests
+    {
+        private IMaterialRepository _materialDBRepository;
+        private Client _someClient;
+        private static RayTracingContext _testContext;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Database.SetInitializer(new DropCreateDatabaseAlways<RayTracingContext>());
+            _testContext = new RayTracingContext();
+            _testContext.Database.Initialize(true);
+            _materialDBRepository = new MaterialDBRepository(_testContext);
+            DateTime testDate = DateTime.Now;
+            const string username = "John";
+            const string password = "Abc12345";
+
+            _someClient = new Client()
+            {
+                Username = username,
+                Password = password,
+                RegisterDate = testDate
+            };
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            _testContext.Dispose();
+        }
+        [TestMethod]
+        public void AddMaterialSuccessfully()
+        {
+            Color materialColor = Color.FromArgb(205, 215, 235);
+            Material someMaterial = new Lambertian()
+            {
+                Proprietary = _someClient,
+                Name = "New Material",
+                Color = materialColor
+            };
+            _materialDBRepository.Add(someMaterial);
+            Assert.AreEqual(someMaterial, _materialDBRepository.GetByName("New Material"));
+        }
+
+    }
+}
