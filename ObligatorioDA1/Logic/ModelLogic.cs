1245135ab6a2b5618f6e8e979597a1425521cdfe
LeanOl <olmedo.leandro8@gmail.com>
2023-06-12 19:11:34 -0300

    [Refactor] Changed model logic tests to use db repository

--- ObligatorioDA1/Logic/ModelLogic.cs
+++ ObligatorioDA1/Logic/ModelLogic.cs
@@ -7,12 +7,17 @@ namespace Logic
 {
     public class ModelLogic
     {
-        private ModelRepository _modelRepository;
+        private IModelRepository _modelRepository;
         private const string DuplicateNameMessage = "There is already a model with this name";
 
         private ModelLogic()
         {
             _modelRepository = new ModelRepository();
+        }
+
+        public ModelLogic(IModelRepository modelRepository)
+        {
+            _modelRepository = modelRepository;
         }
         public static ModelLogic Instance { get; } = new ModelLogic();
 
